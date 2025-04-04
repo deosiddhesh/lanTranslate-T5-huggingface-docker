8c98da419cc5709186573cc9ce37122ffc499c41
Siddhesh <siddheshdeo2000@gmail.com>
2025-04-04 19:45:39 +0530

    update files

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-# this is python file to test
-
 from fastapi import FastAPI
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text2text-generation", model="google/flan-t5-base")
 
 app = FastAPI()
 
@@ -8,3 +10,9 @@ app = FastAPI()
 @app.get('/')
 def home():
     return {"hello": "Bitfumes"}
+
+
+@app.get('/ask')
+def ask(prompt: str):
+    result = pipe(prompt)
+    return result[0]
